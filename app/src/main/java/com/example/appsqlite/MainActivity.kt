d1739c900a6f6dc14c08b2319c641748eed2ed33
mathsant <matheus.santana114@etec.sp.gov.br>
2025-05-16 12:22:34 -0300

    feat: variables has been added for text fields and XML file to first view has been added

--- app/src/main/java/com/example/appsqlite/MainActivity.kt
+++ app/src/main/java/com/example/appsqlite/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.appsqlite
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,32 +19,38 @@ import com.example.appsqlite.ui.theme.AppSQLiteTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Arquivo XML usado para o desenvolvimento da view
+        setContentView(R.layout.activity_main)
+
+        /* Variáveis dos campos que serão adicionados para o salvamento de dados do usuário
+        val edtNome:EditText = findViewById(R.id.edtNome)
+        val edtEndereco:EditText = findViewById(R.id.edtEndereco)
+        val edtBairro:EditText = findViewById(R.id.edtBairro)
+        val edtCep:EditText = findViewById(R.id.edtCep)
+        val edtObservacoes:EditText = findViewById(R.id.edtObservacoes)
+        val edtDataCadastro:EditText = findViewById(R.id.edtDataCadastro)
+
+        val btnCadastrar:Button = findViewById(R.id.btnCadastrar)
+
+        btnCadastrar.setOnClickListener {
+            val intent = Intent(this, RespostaActivity::class.java)
+            intent.putExtra("nome", edtNome.text.toString())
+            intent.putExtra("endereço", edtEndereco.text.toString())
+            intent.putExtra("bairro", edtBairro.text.toString())
+            intent.putExtra("cep", edtCep.text.toString())
+            intent.putExtra("observações", edtObservacoes.text.toString())
+            intent.putExtra("data de cadastro", edtDataCadastro.text.toString())
+            startActivity(intent)
+        }
+        */
+
         enableEdgeToEdge()
         setContent {
             AppSQLiteTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AppSQLiteTheme {
-        Greeting("Android")
     }
 }

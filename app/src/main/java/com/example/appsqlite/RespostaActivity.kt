a10ca1afbf0cd3a1326ccedfb98bb9b00b570f92
mathsant <matheusjorgesantana114@gmail.com>
2025-05-31 17:53:20 -0300

    feat: some variables has been added

--- app/src/main/java/com/example/appsqlite/RespostaActivity.kt
+++ app/src/main/java/com/example/appsqlite/RespostaActivity.kt
@@ -1,8 +1,10 @@
 package com.example.appsqlite
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.appsqlite.db.DBHelper
 
 class RespostaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,9 +17,20 @@ class RespostaActivity : AppCompatActivity() {
         val edtCep: EditText = findViewById(R.id.edtCep)
         val edtObservacoes: EditText = findViewById(R.id.edtObservacoes)
         val edtDataCadastro: EditText = findViewById(R.id.edtDataCadastro)
-        //val btnConfirmar: EditText = findViewById()
+        val btnConfirmar: Button = findViewById(R.id.btnConfirmar)
 
+        edtNome.setText(intent.getStringExtra("nome"))
+        edtEndereco.setText(intent.getStringExtra("endereco"))
+        edtBairro.setText(intent.getStringExtra("bairro"))
+        edtCep.setText(intent.getStringExtra("cep"))
+        edtObservacoes.setText(intent.getStringExtra("observacoes"))
+        edtDataCadastro.setText(intent.getStringExtra("dataCadastro"))
 
+        val db = DBHelper(this, null)
 
+        btnConfirmar.setOnClickListener {
+            db.addPessoa(edtNome.text.toString(), edtEndereco.text.toString(), edtBairro.text.toString(),
+                //edtCep.text.toString())
+        }
     }
 }
